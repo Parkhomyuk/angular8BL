bec4034f085266a7abcfe16ae4b3e74800c0a992
Alexey Parkhomyuk <alexp.p4b@gmail.com>
2019-11-21 16:49:23 +0200

    must repiar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import { 
+  MatButtonModule,
+  MatIconModule,
+  MatDatepickerModule
+ } from '@angular/material';
+import { ReservationsComponent } from './reservations/reservations/reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgSelectModule,
+    NgbModule,
+    NgxMaterialTimepickerModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
