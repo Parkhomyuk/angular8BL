cc5540af8a1104587f8b470f89e147713f0e637e
Alexey Parkhomyuk <alexp.p4b@gmail.com>
2019-11-28 18:14:17 +0200

    problem with style

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+togleActive: any= {
+  home: false,
+  equipment:false
+};
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSideBarClick(){}
+  clickTab(item){
+    console.log('item', this.togleActive);
+    
+    this.togleActive[item] = !this.togleActive[item];       
+}
 }
